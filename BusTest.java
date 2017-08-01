b6408ebb013c0c072299dd108cc0e5c0a6cd7698
Alan McLarney <alanmclarney@gmail.com>
2017-08-01 09:22:12 +0100

    Bugs corrected - not functioning

--- BusTest.java
+++ BusTest.java
@@ -7,7 +7,7 @@ public class BusTest {
 
   @Before
   public void before(){
-    bus = new Bus("#16", 17, 30);
+    bus = new Bus("#16", 17);
   }
 
   @Test
@@ -15,19 +15,19 @@ public class BusTest {
     assertEquals("#16", bus.getName());
   }
 
-  @Test
-  public void hasPassengers(){
-    assertEquals(17, bus.getPassenger());
-  }
+  // @Test
+  // public void hasPassengers(){
+  //   assertEquals(17, bus.getPassenger());
+  // }
 
   @Test
   public void hasWeight(){
-    assertEquals(30, bus.getWeight(), 0.01);
+    assertEquals(17, bus.getWeight(), 0.01);
   }
 
   @Test
   public void readyToHPickUpIfLessThan50Passengers(){
-    assertEquals(true, Bus.readyToPickUp());
+    assertEquals(true, bus.readyToPickUp());
   }
   @Test
   public void busStartsEmpty(){
